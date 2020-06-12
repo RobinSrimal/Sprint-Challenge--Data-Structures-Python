1a7094406269d2289f4a660e7182f605fff4c4f7
Robin Srimal <robin.srimal@gmail.com>
2020-06-12 19:40:19 +0200

    SC with LinkedList

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,36 +1,93 @@
+# solution using an array as storage
+
+# class RingBuffer:
+#     def __init__(self, capacity):
+
+#         self.capacity = capacity
+
+#         self.size = 0
+
+#         self.storage = []
+
+#         self.counter = 0
+               
+
+#     def append(self, item):
+
+#         if self.size < self.capacity:
+
+#             self.storage.append(item)
+
+#             self.size += 1
+
+#         else:
+
+#             self.storage[self.counter] = item
+
+#             if self.counter < self.capacity -1:
+
+#                 self.counter  += 1
+
+#             else:
+
+#                 self.counter = 0
+
+
+#     def get(self):
+        
+#         return [item for item in self.storage if item != None]
+
+
+# solution using a DoublyLinkedList
+
+from doubly_linked_list import DoublyLinkedList
+
+
+
 class RingBuffer:
     def __init__(self, capacity):
 
         self.capacity = capacity
 
-        self.size = 0
+        self.current = None
 
-        self.storage = []
+        self.storage = DoublyLinkedList()
 
-        self.counter = 0
-               
-
+        
+            
     def append(self, item):
 
-        if self.size < self.capacity:
+        if self.storage.length < self.capacity:
 
-            self.storage.append(item)
+            self.storage.add_to_tail(item)
 
-            self.size += 1
+            self.current = self.storage.head
+
 
         else:
 
-            self.storage[self.counter] = item
+            self.current.value = item
 
-            if self.counter < self.capacity -1:
+            if self.current.next == None:
 
-                self.counter  += 1
+                self.current = self.storage.head
 
             else:
 
-                self.counter = 0
-
-
+                self.current = self.current.next
+           
     def get(self):
+
+        values = []
+
+        current = self.storage.head
+
+        while current:
+
+            values.append(current.value)
+
+            current = current.next
+
+        return [value for value in values if value != None]
         
-        return [item for item in self.storage if item != None]
+        
